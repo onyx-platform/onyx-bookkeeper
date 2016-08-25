00a5ba0bdd0ae71fb1682c960bd0954e807f99f8
OnyxBot <michael.drogalis@onyxplatform.org>
2016-08-25 20:25:32 +0000

    Release version 0.9.10.0-beta1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-bookkeeper "0.9.9.1-SNAPSHOT"
+(defproject org.onyxplatform/onyx-bookkeeper "0.9.10.0-beta1"
   :description "Onyx plugin for BookKeeper"
   :url "https://github.com/onyx-platform/onyx-bookkeeper"
   :license {:name "Eclipse Public License"
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.9.10-SNAPSHOT"]]
+                 [org.onyxplatform/onyx "0.9.10-beta1"]]
   :profiles {:dev {:plugins [[lein-set-version "0.4.1"]
                              [lein-update-dependency "0.1.2"]
                              [lein-pprint "1.1.1"]]}
