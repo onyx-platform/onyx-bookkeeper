fcdb8cb2f8def0300842aaec89171dc2406a4c6a
OnyxBot <michael.drogalis@onyxplatform.org>
2016-09-10 16:04:57 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-bookkeeper "0.9.10.0-beta4"
+(defproject org.onyxplatform/onyx-bookkeeper "0.9.10.0-SNAPSHOT"
   :description "Onyx plugin for BookKeeper"
   :url "https://github.com/onyx-platform/onyx-bookkeeper"
   :license {:name "Eclipse Public License"
