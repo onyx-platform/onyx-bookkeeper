9deef50f88ff390502ac759d42599ee1f49b5f33
OnyxBot <michael.drogalis@onyxplatform.org>
2017-06-14 02:10:24 +0000

    Update dependency org.onyxplatform/onyx to version 0.10.0-20170614_020335-g6a57e04.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [org.apache.bookkeeper/bookkeeper-server "4.4.0" :exclusions [org.slf4j/slf4j-log4j12]]
                  ;[org.apache.bookkeeper/bookkeeper "4.4.0" :exclusions [org.slf4j/slf4j-log4j12]]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.10.0-20170614_011859-g0475b38"]]
+                 [org.onyxplatform/onyx "0.10.0-20170614_020335-g6a57e04"]]
   :jvm-opts ^:replace ["-Xmx3g"]
   :profiles {:debug 
              {:jvm-opts ^:replace ["-server"
